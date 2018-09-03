4727e7df93cfed837c5781a6a1b2a70e10428558
LLopes <eulle100@gmail.com>
2018-09-03 18:40:08 -0300

    Aula9

---
+++ Aula9/COntaPoupaca.java
@@ -0,0 +1,14 @@
+public class ContaPoupança{
+    private String cliente;
+    private int num_conta;
+    private double saldo;
+    private int dia_de_rendimento;
+
+    public ContaPoupança( String c, int nc, double s, int dr){}
+    public void sacar(){}
+    public void deposita(){}
+    public String toString(){}
+    public void calcularNovoSaldo(){}
+
+
+}
---
+++ Aula9/ContaBancaria.java
@@ -0,0 +1,11 @@
+public class ContaBancaria{
+    private String cliente;
+    private int num_conta;
+    private double saldo;
+
+    public ContaBancaria( String c, int nc, double s){}
+    public void sacar(){}
+    public void deposita(){}
+    public String toString(){}
+
+}
