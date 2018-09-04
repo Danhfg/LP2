ee1b506227d4b1934d41f49c06c25997b4e3178f
Daniel Henrique Ferreira Gomes <danielhenriquefg@gmail.com>
2018-09-04 08:22:42 -0300

    Q3 lista Aula 11

---
+++ Aula9/Q2/ContaEspecial.java
@@ -0,0 +1,12 @@
+public class ContaEspecial extends ContaBancaria{
+    private String cliente;
+    private int num_conta;
+    private double saldo;
+    private double limite;
+
+    public ContaPoupança( String c, int nc, double s, double l){}
+    public void sacar(){}
+    public String toString(){}
+
+
+}
---
+++ Aula9/Q2/ContaPoupaca.java
@@ -0,0 +1,12 @@
+public class ContaPoupança extends ContaBancaria{
+    private String cliente;
+    private int num_conta;
+    private double saldo;
+    private int dia_de_rendimento;
+
+    public ContaPoupança( String c, int nc, double s, int dr ){}
+    public String toString(){}
+    public void calcularNovoSaldo(){}
+
+
+}
